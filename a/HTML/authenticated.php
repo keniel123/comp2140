0bec157876846bca4a465110f7f95160489c3c2d
sultanofcardio <kevin.lindsay65@gmail.com>
2016-04-04 23:47:36 -0500

    Removed unnecessary shit

--- a/HTML/authenticated.php
+++
@@ -1,4 +0,0 @@
-<li><span><?php echo "Hello ".$_SESSION['username']; ?></span>
-							<ul><li><a href="logout.php">Logout</a></li>
-							<li><a href='settings.php'>Settings</a></li></ul>
-						</li>
--- a/HTML/notlogged.php
+++
@@ -1,5 +0,0 @@
-<li><span>Hello. Sign In</span>
-							<ul><li><button class="btn btn-link"><a href="login.php">Login</a></button></li>
-								<li><button class="btn btn-link"><a href="signup.php">Sign Up</a></button></li></ul>
-						</li>
-						

9e9244d13a68ee55274de05a301af3fe8086843f
Keniel Peart <keniel123@users.noreply.github.com>
2016-04-10 16:19:24 -0500

    Update Account.php

--- models/Merchant/Account.php
+++ models/Merchant/Account.php
@@ -68,7 +68,7 @@ class Account extends User
     public function getFirstName()//:String
     {
         // TODO: implement here
-        return null;
+        return $this->firstName;
     }
 
     /**
@@ -78,7 +78,7 @@ class Account extends User
     public function setFirstName(String $firstName)//:boolean
     {
         // TODO: implement here
-        return false;
+        return $this->firstName = $firstName;
     }
 
     /**
@@ -87,7 +87,7 @@ class Account extends User
     public function getLastName()//:String
     {
         // TODO: implement here
-        return null;
+        return $this->lastName = $lastName;
     }
 
     /**
@@ -97,7 +97,7 @@ class Account extends User
     public function setLastName(String $lastName)//:boolean
     {
         // TODO: implement here
-        return false;
+        return $this->lastName = $lastName;
     }
 
     /**
@@ -106,7 +106,7 @@ class Account extends User
     public function getShippingAddress()//:Address
     {
         // TODO: implement here
-        return null;
+        return $this->shippingAddress
     }
 
     /**
@@ -116,7 +116,7 @@ class Account extends User
     public function setShippingAddress(Address $address)//:boolean
     {
         // TODO: implement here
-        return false;
+        return $this->shippingAddress = $address;
     }
 
     /**
@@ -125,16 +125,16 @@ class Account extends User
     public function getOrders()//:List<Order>
     {
         // TODO: implement here
-        return null;
+        return $this->orders;
     }
 
     /**
      * @return boolean
      */
-    public function updateOrders()//:boolean
+    public function updateOrders($orders)//:boolean
     {
         // TODO: implement here
-        return false;
+        return $this->orders = $orders;
     }
 
     /**
@@ -143,7 +143,7 @@ class Account extends User
     public function getPhoneNumber()//:String
     {
         // TODO: implement here
-        return null;
+        return $this->phoneNumber;
     }
 
     /**
@@ -152,8 +152,7 @@ class Account extends User
      */
     public function setPhoneNumber(String $number)//:boolean
     {
-        // TODO: implement here
-        return false;
+        return $this->phoneNumber = $number
     }
 
     /**
